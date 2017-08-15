8f9da0d13bd239d9536ad1f8d1447b7e46d4a7f9
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-08-15 10:54:51 +0200

    BO-1 : init : create library BOWOListView

---
+++ bowolistview/src/main/java/fr/bowo/bowolistview/BOWOListView.java
@@ -0,0 +1,42 @@
+package fr.bowo.bowolistview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by vincentchann on 15/08/2017.
+ */
+
+public class BOWOListView extends RecyclerView {
+
+    /*
+    Attributes
+     */
+
+
+    /*
+    Constructor
+     */
+    public BOWOListView(Context context) {
+        super(context);
+    }
+
+    public BOWOListView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BOWOListView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /*
+    Public methods
+     */
+
+
+    /*
+    Private methods
+     */
+}
