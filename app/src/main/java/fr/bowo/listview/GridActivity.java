4791000afbfcf6d6732ab2b22e7d7ea8a212040c
Vincent Chann <vc@smartapps.fr>
2017-08-16 17:02:04 +0200

    BO-3 : demo : grid demo multiple span cells

--- app/src/main/java/fr/bowo/listview/GridActivity.java
+++ app/src/main/java/fr/bowo/listview/GridActivity.java
@@ -3,11 +3,10 @@ package fr.bowo.listview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.OrientationHelper;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +25,7 @@ public class GridActivity extends AppCompatActivity implements BOWOListListener 
     Attributes
      */
     protected BOWOListView listView;
+    protected final String KEY_GRID_SPAN_COUNT = "grid_span_count";
 
     /*
     Life cycle
@@ -35,7 +35,7 @@ public class GridActivity extends AppCompatActivity implements BOWOListListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
         listView = (BOWOListView) findViewById(R.id.list_view);
-        listView.start(getDataViewList(), new GridLayoutManager(this, 3), this);
+        listView.start(getDataViewList(), getGridLayoutManager(3), this);
     }
 
     @Override
@@ -43,7 +43,9 @@ public class GridActivity extends AppCompatActivity implements BOWOListListener 
         ImageView cellImage = (ImageView) itemView.findViewById(R.id.cell_image);
 
         if (cellImage != null) {
-
+            Glide.with(this)
+                    .load("file:///android_asset/" + dataView.getImage())
+                    .into(cellImage);
         }
     }
 
@@ -52,11 +54,31 @@ public class GridActivity extends AppCompatActivity implements BOWOListListener 
      */
     protected List<BOWODataView> getDataViewList() {
         List<BOWODataView> result = new ArrayList<>();
-        result.add(new BOWODataView(R.layout.cell_grid).setImage(""));
-        result.add(new BOWODataView(R.layout.cell_grid).setImage(""));
-        result.add(new BOWODataView(R.layout.cell_grid).setImage(""));
-        result.add(new BOWODataView(R.layout.cell_grid).setImage(""));
-        result.add(new BOWODataView(R.layout.cell_grid).setImage(""));
+        result.add(new BOWODataView(R.layout.cell_grid_header).setImage("pokemon_header.jpg").setInt(KEY_GRID_SPAN_COUNT, 3));
+        result.add(new BOWODataView(R.layout.cell_grid).setImage("alakazam.png"));
+        result.add(new BOWODataView(R.layout.cell_grid).setImage("carapuce.png"));
+        result.add(new BOWODataView(R.layout.cell_grid).setImage("evoli.png"));
+        result.add(new BOWODataView(R.layout.cell_grid).setImage("pikachu.png"));
+        result.add(new BOWODataView(R.layout.cell_grid_header).setImage("pokemon_header.jpg").setInt(KEY_GRID_SPAN_COUNT, 2));
+        result.add(new BOWODataView(R.layout.cell_grid).setImage("roocool.png"));
+        return result;
+    }
+
+    protected GridLayoutManager getGridLayoutManager(int span) {
+        GridLayoutManager result = new GridLayoutManager(this, span);
+        final List<BOWODataView> dataViewList = getDataViewList();
+        result.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                BOWODataView currentDataView = dataViewList.get(position);
+                if (currentDataView.getInt(KEY_GRID_SPAN_COUNT) > 0) {
+                    return currentDataView.getInt(KEY_GRID_SPAN_COUNT);
+                }
+                else {
+                    return 1;
+                }
+            }
+        });
         return result;
     }
 
